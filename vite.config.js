17524e21bc5c4d95951a2245d416349f28ddff4d
Walker <walker@praiseweb.com.br>
2021-03-18 11:54:18 -0300

    feat: add eslint and fix lint errors

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
+import windicss from 'vite-plugin-windicss';
 
 export default defineConfig({
-    plugins: [
-        windicss({
-            searchDirs: ['.vitepress/theme']
-        })
-    ]
-})
+  plugins: [
+    windicss({
+      searchDirs: ['.vitepress/theme'],
+    }),
+  ],
+});
