9340cefdc3e260170c476db1ccc4557ffe057f8a
Walker <walker@praiseweb.com.br>
2021-03-31 18:06:16 -0300

    feat: add home head

--- .vitepress/theme/Home.vue
+++ .vitepress/theme/Home.vue
@@ -1,3 +1,7 @@
 <template>
-  <div>Home</div>
+  <div class="flex flex-col items-center py-8 text-center">
+    <img src="/logo.jpg" alt="Praiseweb Logo" class="rounded-full w-24 h-24">
+    <h1 class="text-3xl font-sans font-medium pt-4">{{ $frontmatter.title }}</h1>
+    <h1 class="text-lg font-sans pt-4">{{ $frontmatter.subtext }}</h1>
+  </div>
 </template>
