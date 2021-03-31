a75e468e4729e348c0ceae7303781973ca73f193
Walker <walker@praiseweb.com.br>
2021-03-31 18:19:58 -0300

    feat: add social links

--- .vitepress/theme/Home.vue
+++ .vitepress/theme/Home.vue
@@ -3,5 +3,10 @@
     <img src="/logo.jpg" alt="Praiseweb Logo" class="rounded-full w-24 h-24">
     <h1 class="text-3xl font-sans font-medium pt-4">{{ $frontmatter.title }}</h1>
     <h1 class="text-lg font-sans pt-4">{{ $frontmatter.subtext }}</h1>
+    <div class="pt-4 underline">
+      <a href="https://profile.codersrank.io/user/klarkc" target="_blank">Codersrank</a>
+      <a href="https://twitter.com/klarkc" class="px-4" target="_blank">Twitter</a>
+      <a href="https://github.com/klarkc" target="_blank">GitHub</a>
+    </div>
   </div>
 </template>
