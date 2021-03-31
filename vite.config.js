0333e946d856078f755a0c59dddedee29a6e2cac
Walker <walker@praiseweb.com.br>
2021-03-31 16:49:02 -0300

    fix: vueuse not working correctly

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite';
 import windicss from 'vite-plugin-windicss';
 
 export default defineConfig({
+  optimizeDeps: {
+    exclude: [
+      '@vueuse/core',
+    ],
+  },
   plugins: [
     windicss({
       scan: {
