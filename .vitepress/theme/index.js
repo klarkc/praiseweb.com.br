b6bcbb327ba99c400e04eb850164a695c85d9e45
Walker <walker@praiseweb.com.br>
2021-03-31 16:17:44 -0300

    feat: add devtools windicss

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,5 +1,7 @@
-// eslint-disable-next-line import/no-unresolved
+/* eslint-disable import/no-unresolved */
 import 'virtual:windi.css';
+import 'virtual:windi-devtools';
+/* eslint-enable import/no-unresolved */
 import Layout from './Layout.vue';
 
 export default {
