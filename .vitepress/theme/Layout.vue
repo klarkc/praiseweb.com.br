f1f469f84143658ecc511bf7542b9cdf17e5f609
Walker <walker@praiseweb.com.br>
2021-03-31 21:03:47 -0300

    feat: add breadcrumbs

--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="min-h-screen space-y-12 py-4 bg-yellow-300 text-gray-800 dark:bg-green-900 dark:text-gray-300">
     <div class="container mx-auto px-4 flex">
-      <div class="flex-1" />
+      <div class="flex-1">
+        <a v-if="!isIndex" class="flex items-center text-lg font-medium" href="/">
+          <ArrowLeftIcon class="h-6 w-6 mr-4" /> Home
+        </a>
+      </div>
       <DarkMode />
     </div>
     <main class="max-w-2xl mx-auto px-4">
@@ -15,6 +19,7 @@
 import { computed } from 'vue';
 import { useRoute } from 'vitepress';
 import DarkMode from './DarkMode.vue';
+import { ArrowLeftIcon } from '@heroicons/vue/outline';
 import Home from './Home.vue';
 import Article from './Article.vue';
 
