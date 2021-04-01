7e8d27bbee887a62021e3e6fb52d34d0adb96ce5
Walker <walker@praiseweb.com.br>
2021-03-31 21:18:57 -0300

    feat: add article meta

--- .vitepress/theme/Article.vue
+++ .vitepress/theme/Article.vue
@@ -1,8 +1,9 @@
 <template>
-  <main class="container mx-auto px-4 space-y-8">
+  <main class="container mx-auto px-4">
     <article class="space-y-8">
-      <header>
-        <ArticleTitle class="text-4xl text-center">{{ data.title }}</ArticleTitle>
+      <header class="text-center">
+        <ArticleTitle class="text-4xl">{{ data.title }}</ArticleTitle>
+        <ArticleMeta :date="postCustom.date" :readTime="postCustom.readTime" />
       </header>
       <div>
         <Content class="prose" />
@@ -15,7 +16,17 @@
 </template>
 
 <script setup>
-import { useFrontmatter } from 'vitepress';
+import { useFrontmatter, useSiteData, useRoute } from 'vitepress';
+import { computed } from 'vue';
 import ArticleTitle from './ArticleTitle.vue';
+import ArticleMeta from './ArticleMeta.vue';
+
+function findCurrentIndex() {
+  return posts.findIndex(p => p.href === route.path)
+}
+
 const data = useFrontmatter();
+const route = useRoute();
+const posts = useSiteData().value.customData.posts
+const postCustom = computed(() => posts[findCurrentIndex()])
 </script>
