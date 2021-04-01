05cf436f480f8dd3dfeac534f9e37178af94ac26
Walker <walker@praiseweb.com.br>
2021-03-31 21:35:02 -0300

    refact: toolbar layout

--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -1,12 +1,10 @@
 <template>
   <div class="min-h-screen space-y-12 py-4 bg-yellow-300 text-gray-800 dark:bg-green-900 dark:text-gray-300">
     <div class="container mx-auto px-4 flex">
-      <div class="flex-1">
-        <a v-if="!isIndex" class="flex items-center text-lg font-medium" href="/">
-          <ArrowLeftIcon class="h-6 w-6 mr-4" /> Home
-        </a>
-      </div>
-      <DarkMode />
+      <a v-if="!isIndex" class="flex items-center text-lg font-medium" href="/">
+        <ArrowLeftIcon class="h-6 w-6 mr-4" /> Home
+      </a>
+      <DarkMode class="ml-auto" />
     </div>
     <Home v-if="isIndex" />
     <Article v-else />
