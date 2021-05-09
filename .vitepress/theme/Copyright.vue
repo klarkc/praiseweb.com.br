0cbd893efe009494b097146847d3b3c55534dda5
Walker <walker@praiseweb.com.br>
2021-05-09 15:59:54 -0300

    feat: remove vitepress link

--- .vitepress/theme/Copyright.vue
+++ .vitepress/theme/Copyright.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="text-sm text-center text-gray-600 dark:text-gray-400">
     Copyright @ {{new Date().getFullYear()}} |
-    <a href="https://github.com/klarkc/blog" target="_blank">on Github</a> |
-    <a href="https://vitepress.vuejs.org/" target="_blank">by VitePress</a>
+    <a href="https://github.com/klarkc/blog" target="_blank">on Github</a>
   </div>
 </template>
